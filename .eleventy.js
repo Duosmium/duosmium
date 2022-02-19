448d9497c00d74c98bb69ce79b98a8381789ebd2
Tomi Chen <tctree333@users.noreply.github.com>
2022-02-19 12:06:48 -0800

    copy manifest and service worker

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function (eleventyConfig) {
   // copy files
   eleventyConfig.addPassthroughCopy({ "src/images": "/images" });
+  eleventyConfig.addPassthroughCopy({
+    "src/results/manifest.json": "/results/manifest.json",
+  });
+  eleventyConfig.addPassthroughCopy({
+    "src/results/pwabuilder-sw.js": "/results/pwabuilder-sw.js",
+  });
 
   return {
     markdownTemplateEngine: "njk",
