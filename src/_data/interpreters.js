d8d85559b78bf67c2d69ae1ac5e1eb90890a65c6
Tomi Chen <tctree333@users.noreply.github.com>
2022-02-19 10:51:37 -0800

    add interpreters

---
+++ src/_data/interpreters.js
@@ -0,0 +1,22 @@
+const fs = require("fs/promises");
+
+module.exports = async () => {
+  const sciolyff = (await import("sciolyff")).default;
+
+  const files = (await fs.readdir("./data")).filter((filename) =>
+    filename.match(/^[0-9].*/)
+  );
+  const data = await Promise.all(
+    files.map(async (filename) => {
+      const file = await fs.readFile(`./data/${filename}`, "utf8");
+      return {
+        filename: filename.split(".")[0],
+        file,
+      };
+    })
+  );
+  return data.reduce((acc, { filename, file }) => {
+    acc[filename] = new sciolyff.Interpreter(file);
+    return acc;
+  }, {});
+};
