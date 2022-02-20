26b2776bacbcd4445fa77b80a4d73bcafd588184
Tomi Chen <tctree333@users.noreply.github.com>
2022-02-19 20:54:07 -0800

    add schools pages

--- src/_data/computed.js
+++ src/_data/computed.js
@@ -21,6 +21,73 @@ const generateInterpreters = async () => {
   }, {});
 };
 
+const fullSchoolName = (t) =>
+  [t.school, t.city ? `(${t.city}, ${t.state})` : `(${t.state})`].join(" ");
+
+// from https://stackoverflow.com/questions/13627308/
+const ordinalize = (i) => {
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + "st";
+  }
+  if (j == 2 && k != 12) {
+    return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return i + "rd";
+  }
+  return i + "th";
+};
+
+const getParticipatedTournaments = (school, interpreters) => {
+  return Object.entries(interpreters)
+    .map(([tournament, interpreter]) => {
+      const teams = interpreter.teams.filter(
+        (t) => fullSchoolName(t) === school
+      );
+      if (teams.length === 0) return [];
+
+      return [
+        tournament,
+        teams
+          .map((t) => t.rank)
+          .sort()
+          .map(ordinalize),
+      ];
+    })
+    .reduce((acc, [tournament, ranks]) => {
+      if (!tournament || !ranks) return acc;
+      acc[tournament] = ranks;
+      return acc;
+    }, {});
+};
+
+const schoolsByLetter = (interpreters) =>
+  Object.values(interpreters)
+    // get all schools and stringify them
+    .flatMap((i) => i.teams.map(fullSchoolName))
+    // deduplicate
+    .filter((e, i, s) => s.indexOf(e) === i)
+    // sort alphabetically
+    .sort((a, b) =>
+      a
+        .replaceAll(/[^A-Za-z0-9]/g, "")
+        .localeCompare(b.replaceAll(/[^A-Za-z0-9]/g, ""))
+    )
+    // convert to letter: school: tournament: rank[]
+    .reduce((acc, school) => {
+      if (!acc?.[school[0].toLowerCase()]) {
+        acc[school[0].toLowerCase()] = {};
+      }
+      acc[school[0].toLowerCase()][school] = getParticipatedTournaments(
+        school,
+        interpreters
+      );
+
+      return acc;
+    }, {});
+
 const csvEvents = (interpreters) =>
   Object.values(interpreters)
     .flatMap((i) => i.events.map((e) => e.name))
@@ -47,9 +114,11 @@ const csvSchools = (interpreters) =>
 
 module.exports = async () => {
   const interpreters = await generateInterpreters();
+  const schools = schoolsByLetter(interpreters);
 
   return {
     interpreters,
+    schools,
     csvEvents: csvEvents(interpreters),
     csvSchools: csvSchools(interpreters),
   };
--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,5 +1,60 @@
 const fs = require("fs");
 
+const STATES_BY_POSTAL_CODE = {
+  AK: "Alaska",
+  AZ: "Arizona",
+  AR: "Arkansas",
+  CA: "California",
+  nCA: "Northern California",
+  sCA: "Southern California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DE: "Delaware",
+  DC: "District of Columbia",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  IA: "Iowa",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  ME: "Maine",
+  MD: "Maryland",
+  MA: "Massachusetts",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MS: "Mississippi",
+  MO: "Missouri",
+  MT: "Montana",
+  NE: "Nebraska",
+  NV: "Nevada",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NY: "New York",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VT: "Vermont",
+  VA: "Virginia",
+  WA: "Washington",
+  WV: "West Virginia",
+  WI: "Wisconsin",
+  WY: "Wyoming",
+};
+
 const IMAGES_PATH = "./src/images/";
 
 function findLogoPath(filename) {
@@ -35,6 +90,28 @@ function findLogoPath(filename) {
   return "/results/images/logos/" + selected;
 }
 
+function tournamentTitle(tInfo) {
+  if (tInfo.name) return tInfo.name;
+
+  switch (tInfo.level) {
+    case "Nationals":
+      return "Science Olympiad National Tournament";
+    case "States":
+      return `${expandStateName(
+        tInfo.state
+      )} Science Olympiad State Tournament`;
+    case "Regionals":
+      return `${tInfo.location} Regional Tournament`;
+    case "Invitational":
+      return `${tInfo.location} Invitational`;
+  }
+}
+
+function expandStateName(postalCode) {
+  return STATES_BY_POSTAL_CODE[postalCode];
+}
+
 module.exports = {
   findLogoPath,
+  tournamentTitle,
 };
