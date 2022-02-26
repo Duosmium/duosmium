90360db8c577350b7aadfa28e7f8d7b69d2a7a4e
Tomi Chen <tctree333@users.noreply.github.com>
2022-02-26 10:31:45 -0800

    basic sanitization

--- src/_data/interpreter.js
+++ src/_data/interpreter.js
@@ -4,8 +4,10 @@ module.exports = async () => {
   const sciolyff = (await import("sciolyff")).default;
 
   return (filename, urlPath) => {
+    if (/[^A-Za-z0-9_\-]/.test(filename)) {
+      throw new Error(`Invalid filename: ${filename}`);
+    }
     const file = fs.readFileSync(`./data/${filename}.yaml`, "utf8");
-    console.log(urlPath);
     if (urlPath.includes("/superscore/")) {
       return new sciolyff.Interpreter(
         new sciolyff.Interpreter(file).superscore()
