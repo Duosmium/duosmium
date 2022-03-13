7acf470987dc398b4dae43f47076edff16730984
Tomi Chen <tctree333@users.noreply.github.com>
2022-03-13 14:12:08 -0700

    update template to be more flexible

--- src/_data/interpreter.js
+++ src/_data/interpreter.js
@@ -3,14 +3,16 @@ const fs = require("fs");
 module.exports = async () => {
   const sciolyff = (await import("sciolyff")).default;
 
-  return (filename, urlPath) => {
-    if (/[^A-Za-z0-9_\-]/.test(filename)) {
-      throw new Error(`Invalid filename: ${filename}`);
-    }
-    const file = fs.readFileSync(`./data/${filename}.yaml`, "utf8");
-    if (urlPath.includes("/superscore/")) {
-      return new sciolyff.Interpreter(file).superscore(true);
-    }
-    return new sciolyff.Interpreter(file);
+  return {
+    fromFilename: (filename, superscore) => {
+      if (/[^A-Za-z0-9_\-]/.test(filename)) {
+        throw new Error(`Invalid filename: ${filename}`);
+      }
+      const file = fs.readFileSync(`./data/${filename}.yaml`, "utf8");
+      if (superscore) {
+        return new sciolyff.Interpreter(file).superscore(true);
+      }
+      return new sciolyff.Interpreter(file);
+    },
   };
 };
