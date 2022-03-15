b5cfce4e55f1e1949c6c0d2198b5892c4da16af0
Tomi Chen <tctree333@users.noreply.github.com>
2022-03-14 22:12:15 -0700

    remove logging
    [skip ci]

--- serverless/dynamicpost/index.js
+++ serverless/dynamicpost/index.js
@@ -5,7 +5,6 @@ const { EleventyServerless } = require("@11ty/eleventy");
 require("./eleventy-bundler-modules.js");
 
 async function handler(event) {
-  console.log(event);
   let data = {};
   if (event.path === "/preview/render/") {
     if (event.httpMethod !== "POST") {
