22c2abd7fd9540661b80c8e10253b68eaf0adff2
Tomi Chen <tctree333@users.noreply.github.com>
2023-01-08 14:57:13 -0800

    update placeholders for active tournament support

--- src/_data/official.js
+++ src/_data/official.js
@@ -1,9 +1,32 @@
+const yaml = require("js-yaml");
+const fs = require("fs/promises");
+const { escapeCsv } = require("../../utils/helpers");
+
+const getActiveTournaments = async () => {
+  const sciolyff = (await import("sciolyff")).default;
+
+  const files = (await fs.readdir("./data/active")).filter((filename) =>
+    /^[0-9].*/.test(filename)
+  );
+  const tournaments = await Promise.all(
+    files.map(async (filename) => {
+      const file = await fs.readFile(`./data/active/${filename}`, "utf8");
+      return [filename.split(".")[0], new sciolyff.Interpreter(file)];
+    })
+  );
+  return [tournaments.map((t) => t[0]), tournaments];
+};
+
+const teamsCsv = (interpreter) =>
+  interpreter.teams
+    .map((team) => [team.number, team.school, team.suffix].map(escapeCsv))
+    .sort((a, b) => a[0] - b[0])
+    .map((team) => team.join(","))
+    .join("\n");
+
 module.exports = async () => {
   // don't run on serverless requests
   // if (process.env.ELEVENTY_SERVERLESS) return;
-
-  const yaml = require("js-yaml");
-  const fs = require("fs/promises");
 
   try {
     const fileContents = await fs.readFile("./data/official.yaml", "utf8");
@@ -11,12 +34,27 @@ module.exports = async () => {
 
     if (!(doc instanceof Array)) return { all: [], placeholder: [] };
 
-    const hasResults = (await fs.readdir("./data/results")).flatMap((filename) =>
-      /^[0-9].*/.test(filename) ? [filename.split(".")[0]] : []
+    const hasResults = (await fs.readdir("./data/results")).flatMap(
+      (filename) => (/^[0-9].*/.test(filename) ? [filename.split(".")[0]] : [])
     );
+
+    const placeholder = doc.filter(
+      (tournament) => !hasResults.includes(tournament)
+    );
+    const [activeNames, activeTournaments] = await getActiveTournaments();
+
+    const tournaments = activeTournaments.slice();
+    placeholder.forEach((filename) => {
+      if (!activeNames.includes(filename)) {
+        tournaments.push([filename, undefined]);
+      }
+    });
+
     return {
       all: doc,
-      placeholder: doc.filter((tournament) => !hasResults.includes(tournament)),
+      placeholder: tournaments,
+      active: activeTournaments,
+      teamsCsv,
     };
   } catch (e) {
     console.error(e);
