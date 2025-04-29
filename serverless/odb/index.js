2452212693af6d28f9083b86bc867c1c2e160e0f
Tomi Chen <tctree333@users.noreply.github.com>
2025-04-29 10:42:17 -0700

    remove logging

--- serverless/odb/index.js
+++ serverless/odb/index.js
@@ -30,7 +30,6 @@ async function handler(event) {
     // If you want some of the data cascade available in `page.data`, use `eleventyConfig.dataFilterSelectors`.
     // Read more: https://www.11ty.dev/docs/config/#data-filter-selectors
     console.log("Built page: " + event.path);
-    console.dir(event.headers);
 
     return {
       statusCode: 200,
@@ -70,7 +69,7 @@ async function handler(event) {
 // * Runs on first request only: Netlify On-demand Builder
 //   (don’t forget to `npm install @netlify/functions`)
 
-exports.handler = handler;
+// exports.handler = handler;
 
-// const { builder } = require("@netlify/functions");
-// exports.handler = builder(handler);
+const { builder } = require("@netlify/functions");
+exports.handler = builder(handler);
